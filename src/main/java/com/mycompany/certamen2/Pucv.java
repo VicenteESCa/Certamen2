de7b41286fca2ad5b55312b0ae144cba131fac1d
VicenteESCa <149270542+VicenteESCa@users.noreply.github.com>
2023-11-17 15:34:44 -0300

    Update Pucv.java

--- src/main/java/com/mycompany/certamen2/Pucv.java
+++ src/main/java/com/mycompany/certamen2/Pucv.java
@@ -8,6 +8,29 @@ package com.mycompany.certamen2;
  *
  * @author Estudiante
  */
-public class Pucv {
+public class Pucv extends libro{
+    private String institucion;
+
+    public Pucv(String institucion, String titulo, String autor, int codigo, int precio) {
+        super(titulo, autor, codigo, precio);
+        this.institucion = institucion;
+    }
+
+    public String getInstitucion() {
+        return institucion;
+    }
+
+    public void setInstitucion(String institucion) {
+        this.institucion = institucion;
+    }
+
     
+    @Override
+    public String consultarTipo() {
+        return "Tipo: libro de la PUCV";
+    }
+    
+    public void CrearDato(){
+        new Pucv("Ing Informatica","POO","yo",777,2500);
+    }
 }
