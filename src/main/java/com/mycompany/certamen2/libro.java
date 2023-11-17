46722807067b67b9268b23dbb7b7bde0aa494fa4
richar776 <151217230+richar776@users.noreply.github.com>
2023-11-17 14:54:23 -0300

    Update libro.java

--- src/main/java/com/mycompany/certamen2/libro.java
+++ src/main/java/com/mycompany/certamen2/libro.java
@@ -4,10 +4,23 @@
  */
 package com.mycompany.certamen2;
 
-/**
- *
- * @author Estudiante
- */
 public class libro {
+    
+    private String titulo;
+    private String autor;
+    private int codigo;
+    private int precio;
+    private int tipo;
+
+    public libro(String titulo, String autor, int codigo, int precio, int tipo) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.codigo = codigo;
+        this.precio = precio;
+        this.tipo = tipo;
+    }
+    
+    
+    
     
 }
