989dbdfdd3cae9dca330aa9cb2dc48e9215b2bb9
VicenteESCa <149270542+VicenteESCa@users.noreply.github.com>
2023-11-17 15:00:30 -0300

    Update libro.java

--- src/main/java/com/mycompany/certamen2/libro.java
+++ src/main/java/com/mycompany/certamen2/libro.java
@@ -4,20 +4,50 @@
  */
 package com.mycompany.certamen2;
 
-public class libro {
+public abstract class libro {
     
     private String titulo;
     private String autor;
     private int codigo;
     private int precio;
-    private int tipo;
 
     public libro(String titulo, String autor, int codigo, int precio, int tipo) {
         this.titulo = titulo;
         this.autor = autor;
         this.codigo = codigo;
         this.precio = precio;
-        this.tipo = tipo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
     }
     
     
